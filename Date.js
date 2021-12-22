cd5f91a9598b63012a90530cbf67b1ef04c3bcc7
unknown <sangnhanma14@gmail.com>
2021-12-22 10:36:07 +0700

    add index.html file

---
+++ Date.js
@@ -0,0 +1,28 @@
+class Date {
+    day;
+    month;
+    year;
+    constructor(day,month,year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    getDay(){
+        return this.day;
+    }
+    setDay(day){
+        this.day = day;
+    }
+    getMonth(){
+        return this.month;
+    }
+    setMonth(month){
+        this.month = month;
+    }
+    getYear(){
+        return this.year;
+    }
+    setYear (year){
+        this.year = year;
+    }
+}
